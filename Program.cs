e829ee8f54cc76c400a84337d7e30621aee8bf7e
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2021-10-03 17:54:53 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace torflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Tests1.TorFlow(args);
+            Application.Run();
+        }
+    }
+}
